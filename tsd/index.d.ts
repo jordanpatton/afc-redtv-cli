34b0d85adf1b302bdbf9d955ece774c5b512e391
Jordan Patton <jordanpatton@gmail.com>
2025-04-13 19:49:12 -0400

    add tsd folder

---
+++ tsd/index.d.ts
@@ -0,0 +1,6 @@
+// This file declares modules that should not be type-checked. Example:
+// declare module 'module-without-type-checking';
+// It also declares modules with custom types and overrides. Example:
+// declare module 'module-with-custom-types' {
+//     export function doSomething(input: string): string;
+// }
