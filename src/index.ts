bc830493ab43f064e6ba898a83d1eae171ee5a23
Jordan Patton <jordanpatton@gmail.com>
2025-04-13 20:00:38 -0400

    add stub entrypoint

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Main entrypoint for this program.
+ * 
+ * @returns Command result or void.
+ */
+export const main = (): void => {
+    throw new Error('Not yet implemented.');
+};
+
+main();
